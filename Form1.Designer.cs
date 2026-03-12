6196e0b297158b2bd9d45140880c6a3a60fb21d9
brocallre <hyt83532342@gmail.com>
2026-03-12 11:26:21 +0900

    1단계: 기본 도망 기능 구현
    - 버튼 컨트롤 추가 (텍스트: "나를 잡아봐")
    - 마우스가 버튼 위로 이동하면 랜덤 위치로 도망
    - 폼 제목에 버튼의 X, Y 좌표 실시간 표시
    - 버튼 이동 범위 제한 (폼 밖으로 나가지 않도록)
    - 마우스 포인터를 Hand 모양으로 변경

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace CatchButton
+namespace CatchButton
 {
     partial class Form1
     {
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            this.btnCatch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            //
+            // btnCatch - 잡아야 할 버튼
+            //
+            this.btnCatch.Location = new System.Drawing.Point(300, 200);
+            this.btnCatch.Name = "btnCatch";
+            this.btnCatch.Size = new System.Drawing.Size(120, 40);
+            this.btnCatch.TabIndex = 0;
+            this.btnCatch.Text = "나를 잡아봐";
+            this.btnCatch.UseVisualStyleBackColor = true;
+            this.btnCatch.MouseEnter += new System.EventHandler(this.btnCatch_MouseEnter);
+            this.btnCatch.Click += new System.EventHandler(this.btnCatch_Click);
+            //
+            // Form1 - 메인 폼
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCatch);
+            this.Name = "Form1";
+            this.Text = "숨바꼭질 버튼";
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCatch;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,40 @@ namespace CatchButton
 {
     public partial class Form1 : Form
     {
+        // 랜덤 위치 생성을 위한 Random 객체
+        private Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 마우스가 버튼 영역에 들어오면 버튼을 랜덤 위치로 이동시킨다
+        /// </summary>
+        private void btnCatch_MouseEnter(object sender, EventArgs e)
+        {
+            // 버튼이 폼 밖으로 나가지 않도록 이동 범위 제한
+            int maxX = this.ClientSize.Width - btnCatch.Width;
+            int maxY = this.ClientSize.Height - btnCatch.Height;
+
+            // 랜덤한 새 좌표 계산
+            int newX = random.Next(0, maxX);
+            int newY = random.Next(0, maxY);
+
+            // 버튼 위치 변경
+            btnCatch.Location = new Point(newX, newY);
+
+            // 폼 제목에 버튼의 현재 좌표 표시
+            this.Text = $"숨바꼭질 버튼 - 버튼 위치: X={newX}, Y={newY}";
+        }
+
+        /// <summary>
+        /// 버튼을 클릭하면 축하 메시지를 표시한다
+        /// </summary>
+        private void btnCatch_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("축하합니다~!", "버튼 잡기", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
