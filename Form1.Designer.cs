ee85f4acc58cd58ac558ed6027adf55cbd0c64f6
brocallre <hyt83532342@gmail.com>
2026-03-12 11:48:30 +0900

    Catch_Me_SizeUp

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,43 +28,48 @@ namespace CatchButton
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnCatch = new System.Windows.Forms.Button();
-            this.btnRestart = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            //
-            // btnCatch - 잡아야 할 버튼
-            //
-            this.btnCatch.Location = new System.Drawing.Point(300, 200);
-            this.btnCatch.Name = "btnCatch";
-            this.btnCatch.Size = new System.Drawing.Size(120, 40);
-            this.btnCatch.TabIndex = 0;
-            this.btnCatch.Text = "나를 잡아봐";
-            this.btnCatch.UseVisualStyleBackColor = true;
-            this.btnCatch.MouseEnter += new System.EventHandler(this.btnCatch_MouseEnter);
-            this.btnCatch.Click += new System.EventHandler(this.btnCatch_Click);
-            //
-            // btnRestart - 다시 시작 버튼
-            //
-            this.btnRestart.Location = new System.Drawing.Point(340, 400);
-            this.btnRestart.Name = "btnRestart";
-            this.btnRestart.Size = new System.Drawing.Size(120, 35);
-            this.btnRestart.TabIndex = 1;
-            this.btnRestart.Text = "다시 시작";
-            this.btnRestart.UseVisualStyleBackColor = true;
-            this.btnRestart.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
-            //
-            // Form1 - 메인 폼
-            //
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnCatch);
-            this.Controls.Add(this.btnRestart);
-            this.Name = "Form1";
-            this.Text = "숨바꼭질 버튼";
-            this.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ResumeLayout(false);
+            btnCatch = new Button();
+            btnRestart = new Button();
+            SuspendLayout();
+            // 
+            // btnCatch
+            // 
+            btnCatch.BackColor = Color.RosyBrown;
+            btnCatch.Font = new Font("맑은 고딕", 23F);
+            btnCatch.Location = new Point(729, 547);
+            btnCatch.Margin = new Padding(7, 8, 7, 8);
+            btnCatch.Name = "btnCatch";
+            btnCatch.Size = new Size(418, 223);
+            btnCatch.TabIndex = 0;
+            btnCatch.Text = "Catch Me";
+            btnCatch.UseVisualStyleBackColor = false;
+            btnCatch.Click += btnCatch_Click;
+            btnCatch.MouseEnter += btnCatch_MouseEnter;
+            // 
+            // btnRestart
+            // 
+            btnRestart.Anchor = AnchorStyles.Bottom;
+            btnRestart.Location = new Point(826, 1093);
+            btnRestart.Margin = new Padding(7, 8, 7, 8);
+            btnRestart.Name = "btnRestart";
+            btnRestart.Size = new Size(291, 96);
+            btnRestart.TabIndex = 1;
+            btnRestart.Text = "다시 시작";
+            btnRestart.UseVisualStyleBackColor = true;
+            btnRestart.Click += btnRestart_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1943, 1230);
+            Controls.Add(btnCatch);
+            Controls.Add(btnRestart);
+            Cursor = Cursors.Hand;
+            Margin = new Padding(7, 8, 7, 8);
+            Name = "Form1";
+            Text = "숨바꼭질 버튼";
+            ResumeLayout(false);
         }
 
         #endregion
